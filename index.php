c89554b37872a479d13c20631459e6fcf9d43341
jude <jude.mbengani@hotmail.fr>
2017-04-24 12:30:05 +0200

    fini

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Accueil</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="CSS/accueil.css">
+  </head>
+  <body>
+    <div class="mesonglet">
+      <h2>Connexion </h2>
+      <div class="form-group">
+        <input class="form-control" class="D" placeholder="Identifiant" type="text" name="name" value=""><br>
+      </div>
+      <div class="form-group">
+        <input class="form-control" placeholder="Mot de passe" type="password" name="Prenom" value="">
+      </div>
+      <button type="button" name="button">Connexion</button><br>
+      <a href="#">Inscription ?</a>
+      <a href="#">Mot de passe oublié ?</a>
+    </div>
+  </body>
+</html>
