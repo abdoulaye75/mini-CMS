9c1c9cd18440c0ef5cb4d45d6810ab08ec35ee79
abdoulaye <abdoulayedabo@laposte.net>
2017-04-24 14:30:24 +0200

    liste des articles

---
+++ views/articles.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Les articles </title>
+	<link rel="stylesheet" type="text/css" href="../CSS/articles.css">
+	<link rel="stylesheet" type="text/css" href="../CSS/bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+
+	<table class="table table-bordered table-responsive col-md-6">
+		<thead>
+			<tr>
+				<th> Nom du plat </th>
+				<th> Ingrédients </th>
+				<th> Temps de préparation </th>
+			</tr>
+		</thead>
+
+		<tbody>
+			<tr>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+		</tbody>
+	</table>
+
+</body>
+</html>
