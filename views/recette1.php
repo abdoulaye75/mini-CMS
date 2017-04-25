c247dad2551648c4ea35bd410fa33122e1d90eb7
abdoulaye <abdoulayedabo@laposte.net>
2017-04-25 10:30:50 +0200

    1er exemple de recette

---
+++ views/recette1.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Recette 1</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+    <nav class="navbar navbar-inverse">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>                        
+          </button>
+
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+          <ul class="nav navbar-nav">
+            <li><a href="../index.php"> Accueil</a></li>            
+            <li><a href="articles.php"> Liste des articles </a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="signup.php"> S'inscrire </a></li>
+            <li><a href="signin.php"> se connecter </a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+	<h1>Recette du gâteau</h1>
+	<ol>
+		<li> Casser les oeufs </li>
+		<li> Les verser dans un bol et mélanger </li>
+		<li> Ajouter la farine </li>
+	</ol>
+
+</body>
+</html>
