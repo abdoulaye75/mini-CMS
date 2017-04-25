87aba47156fb4b2d4ecd1cce8fccda7b80977d39
abdoulaye <abdoulayedabo@laposte.net>
2017-04-25 10:27:40 +0200

    création de la base de données

---
+++ database/mini-cms.sql
@@ -0,0 +1,98 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4deb1
+-- https://www.phpmyadmin.net/
+--
+-- Client :  localhost:3306
+-- Généré le :  Mar 25 Avril 2017 à 10:23
+-- Version du serveur :  5.7.17-0ubuntu0.16.10.1
+-- Version de PHP :  7.0.15-0ubuntu0.16.10.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `mini-cms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `recettes`
+--
+
+CREATE TABLE `recettes` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `ingredients` varchar(255) NOT NULL,
+  `preparation_time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `recettes`
+--
+
+INSERT INTO `recettes` (`id`, `name`, `ingredients`, `preparation_time`) VALUES
+(1, 'crêpe', 'farine, oeufs, lait, sel, sucre vanillé, sucre en poudre', '00:02:00'),
+(2, 'gâteau au chocolat', 'chocolat noir, beurre, oeufs, farine, sucre en poudre', '00:40:00'),
+(3, 'madeleine', 'oeufs, sucre, farine, fleur d\'oranger, levure chimique, beurre, lait', '00:45:00'),
+(4, 'purée de pommes de terre', 'pommes de terre, lait, beurre, noix de muscade, sel, poivre', '00:40:00'),
+(5, 'pot-au-feu', 'viande de boeuf, os à moelle, poireau, carottes, branche de céléri, oignons, gousse d\'ail, persil, thym, laurier, clous de girofle, gros sel, poivre noir', '04:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `password`) VALUES
+(1, 'abdoulaye', 'paris18');
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `recettes`
+--
+ALTER TABLE `recettes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `recettes`
+--
+ALTER TABLE `recettes`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
