7d9e53d9579a9c2ea77cdd1bb3f24b9cdb210fbc
abdoulaye <abdoulayedabo@laposte.net>
2017-04-27 17:29:03 +0200

    ajout d'une image pour une fiche recette

--- views/gateau.php
+++ views/gateau.php
@@ -4,9 +4,46 @@
     <meta charset="utf-8">
     <title>gateau au chocolat</title>
     <link rel="stylesheet" type="text/css" href="../CSS/gateau.css">
+	<link rel="stylesheet" type="text/css" href="../CSS/bootstrap/css/bootstrap.min.css">
   </head>
   <body>
+
+	<nav class="navbar navbar-inverse">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>                        
+          </button>
+
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+          <ul class="nav navbar-nav">
+            <li> <a href="../index.php"> Retour à l'accueil </a> </li>
+            <li> <?php if ((isset($_SESSION['name'])) && (isset($_SESSION['password']))) {
+				echo '<a href="create.php"> Ajouter une recette </a>'; ?>
+			</li>
+			<li> <?php echo '<a href="update.php"> Modifier une recette </a>'; ?> </li>
+			<li> <?php echo '<a href="signout.php"> Se déconnecter </a>'; ?> </li>
+			<?php }
+
+			else { ?>
+	    	<li> <?php echo "<a href='signup.php'> S'inscrire </a>"; ?> </li>
+	        <li> <?php echo "<a href='signin.php'> se connecter </a>"; ?> </li>
+
+			<?php }?>    	
+    		<li> <a href="recettes.php"> Fiches recette </a> </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
     <p>Pour faire un gateau il faut !</p>
     <p>farine, oeufs, lait, sel, sucre vanillé, sucre en poudre</p>
+    <img src="../img/cake_chocolate.jpeg" alt="gâteau au chocolat">
+
+  	<script src="../jquery-2.2.4.js"></script>
+	<script src="../CSS/bootstrap/js/bootstrap.min.js"></script>
   </body>
 </html>
--- views/potaufeu.php
+++ views/potaufeu.php
@@ -4,11 +4,48 @@
     <meta charset="utf-8">
     <title>pot-au-feu</title>
     <link rel="stylesheet" type="text/css" href="../CSS/potaufeu.css">
+    <link rel="stylesheet" type="text/css" href="../CSS/bootstrap/css/bootstrap.min.css">
   </head>
   <body>
+
+  <nav class="navbar navbar-inverse">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>                        
+          </button>
+
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+          <ul class="nav navbar-nav">
+            <li> <a href="../index.php"> Retour à l'accueil </a> </li>
+            <li> <?php if ((isset($_SESSION['name'])) && (isset($_SESSION['password']))) {
+        echo '<a href="create.php"> Ajouter une recette </a>'; ?>
+      </li>
+      <li> <?php echo '<a href="update.php"> Modifier une recette </a>'; ?> </li>
+      <li> <?php echo '<a href="signout.php"> Se déconnecter </a>'; ?> </li>
+      <?php }
+
+      else { ?>
+        <li> <?php echo "<a href='signup.php'> S'inscrire </a>"; ?> </li>
+          <li> <?php echo "<a href='signin.php'> se connecter </a>"; ?> </li>
+
+      <?php }?>     
+        <li> <a href="recettes.php"> Fiches recette </a> </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
     <p>les ingrédients !</p>
     <p>viande de boeuf, os à moelle, poireau, carottes, branche de céléri, oignons,
        gousse d'ail, persil, thym, laurier, clous de girofle, gros sel, poivre noir
     </p>
+    <img src="../img/potaufeu.jpeg" alt="pot-au-feu">
+  
+  <script src="../jquery-2.2.4.js"></script>
+  <script src="../CSS/bootstrap/js/bootstrap.min.js"></script>
   </body>
 </html>
