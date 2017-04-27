4b399442c6a56f5876da56930d90adcc14617851
abdoulaye <abdoulayedabo@laposte.net>
2017-04-27 10:28:36 +0200

    correction du bloc if en tête du fichier

--- views/signin.php
+++ views/signin.php
@@ -11,23 +11,20 @@ include '../database/database.php';
 	 $req->execute(array('name' => $login,'password' => $mdp));
 
 	 $connecteduser = $req->fetch();
-}
 	//si les identifiants de l'utilisateur ne figurent pas dans la base de données, on empêche la connexion et on le propose de s'inscrire
-if (!$connecteduser) {
-	echo "Utilisateur inconnu ! Vérifiez bien votre identifiant et votre mot de passe !";
-	echo '<a href="signup.php"> S\'inscrire </a>';
+  if (!$connecteduser) {
+  	echo "Utilisateur inconnu ! Vérifiez bien votre identifiant et votre mot de passe !";
+  	echo '<a href="signup.php"> S\'inscrire </a>';
+  }
+  else { // sinon, la session peut démarrer et l'utilisateur peut accéder à sa page membre personnelle
+  	session_start();
+  	$_SESSION['name'] = $connecteduser['name'];
+  	$_SESSION['mdp'] = $connecteduser['mdp'];
+  	header("Location: http://localhost/mini-CMS/views/page_membre.php");
+  }
 }
-else { // sinon, la session peut démarrer et l'utilisateur peut accéder à sa page membre personnelle
-	session_start();
-	$_SESSION['name'] = $connecteduser['name'];
-	$_SESSION['mdp'] = $connecteduser['mdp'];
-	header("Location: http://localhost/mini-CMS/views/page_membre.php");
-}
-<<<<<<< HEAD
+
  ?>
-=======
-?>
->>>>>>> c7230cd099dd896846ba98f0e3feb7256e30f617
 <!DOCTYPE html>
 <html>
 <head>
