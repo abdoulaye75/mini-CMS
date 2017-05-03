a5aa05741dd08122fd4c7c174ff2e768a968982c
abdoulaye <abdoulayedabo@laposte.net>
2017-05-03 13:58:08 +0200

    ajout d'une variable qui vérifie l'état de connexion

--- index2.php
+++ index2.php
@@ -6,4 +6,8 @@ require 'model.php';
 
 $articles = getArticles();
 
+$user_is_connected = isset($_SESSION['name'], $_SESSION['password']);
+//$user_is_connected = true;
+//$user_is_connected = false;
+
 require 'view.php';
